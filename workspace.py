dc7c7682801230f98d33931c686b00f234037fe4
mpco <aizaitianwai@gmail.com>
2019-04-13 22:43:37 +0800

    Add feature: open and save workspace

---
+++ workspace.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import ast
+import json
+import shlex
+import subprocess
+
+cmdScript = "osascript -e 'tell application \"DEVONthink Pro\" to get workspaces' -s s"
+cmdList = shlex.split(cmdScript)
+cmdResult = subprocess.check_output(cmdList)
+workspaceList = ast.literal_eval(
+    "[" + cmdResult[1:-2].decode("UTF8") + "]")
+
+result = {"items": []}
+for item in workspaceList:
+    temp = {
+        "title": item,
+        "subtitle": "",
+        "arg": item
+    }
+    result['items'].append(temp)
+
+if result['items']:
+    print(json.dumps(result))
+else:
+    # when result list is empty
+    print('{"items": [{"title": "None Workspace","subtitle": "(*´･д･)?"}]}')
