eeb946963a0b5f7346c1c653f1825b261474366d
mpco <aizaitianwai@gmail.com>
2019-05-01 19:27:49 +0800

    Update getTagsList.py, use applescript instead of parse .dtTags files

--- getTagsList.py
+++ getTagsList.py
@@ -1,40 +1,31 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-import os
-import sys
-import json
-
-selectedDbUUID = os.getenv('selectedDbUUID', "")
-
 # todo: if not selectedDbUUID，显示所有 tags
 # 多个 tag 匹配
 
-dtTagsFilePath = os.path.expanduser(
-    "~/Library/Caches/Metadata/DEVONthink Pro 2/Lookup/{}.dtTags".format(selectedDbUUID))
+import os
+import sys
+import json
+import ast
+import shlex
+import subprocess
 
+selectedDbUUID = os.getenv('selectedDbUUID', "")
 
-if not os.path.exists(dtTagsFilePath):
-    sys.stdout.write(json.dumps({"items": [{"title": "Tags File for this database not exists"}]}))
-    sys.exit()
+cmdScript = "osascript -e 'tell application \"DEVONthink Pro\" to get name of tag group of (get database with uuid \"{}\")' -s s".format(selectedDbUUID)
+cmdList = shlex.split(cmdScript)
+cmdResult = subprocess.check_output(cmdList)
+tagList = ast.literal_eval(
+    "[" + cmdResult[1:-2].decode("UTF8") + "]")
 
 result = {"items": []}
-if dtTagsFilePath[-6:] == "dtTags":
-    with open(dtTagsFilePath, 'rb') as f:
-        r = f.read()
-    if not r:
-        sys.stdout.write(json.dumps({"items": [{"title": "No tag in the database"}]}))
-        sys.exit()
-
-    rList = r.split("DTstTAGS")
-
-    for tagStr in rList[1:]:
-        tag = tagStr[8:]
-        result["items"].append({
-            "title": tag,
-            "subtitle": "Enter to list all files with this tag",
-            # "arg": tag,
-            "variables": {"selectedTag": tag, "selectedDbUUID": selectedDbUUID}
-        })
+for tag in tagList:
+    result["items"].append({
+        "title": tag,
+        "subtitle": "Press Enter to list all files with this tag",
+        # "arg": tag,
+        "variables": {"selectedTag": tag, "selectedDbUUID": selectedDbUUID}
+    })
 
 sys.stdout.write(json.dumps(result))
